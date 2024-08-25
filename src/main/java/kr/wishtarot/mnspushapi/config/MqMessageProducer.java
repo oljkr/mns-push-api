6008f96bf5bc6a3e958d47b7f5d4882ef8a28737
Eunju Jeong <zaoljkr@gmail.com>
2024-08-26 01:57:23 +0900

    mq에 produce하는 부분 카멜 케이스로 통일하도록 변경

--- src/main/java/kr/wishtarot/mnspushapi/config/MqMessageProducer.java
+++ src/main/java/kr/wishtarot/mnspushapi/config/MqMessageProducer.java
@@ -18,8 +18,8 @@ public class MqMessageProducer {
     public void sendPushNotification(String queueName, String pushInfoNo, String targetUser, String title, String body, String targetPage, String messageId) {
         // 메시지에 포함될 데이터를 Map에 저장
         Map<String, Object> message = Map.of(
-                "push_info_no", pushInfoNo,
-                "target_user", targetUser,
+                "pushInfoNo", pushInfoNo,
+                "targetUser", targetUser,
                 "title", title,
                 "body", body,
                 "targetPage", targetPage,
@@ -29,4 +29,5 @@ public class MqMessageProducer {
         // 메시지를 JSON 형식으로 변환하여 RabbitMQ로 전송
         rabbitTemplate.convertAndSend(queueName, message);
     }
+
 }
